aeeed9189850b86fa2eef9b20a0195e00c24e40e
byAlexks <alexanderksgamer@gmail.com>
2021-07-24 19:30:28 -0500

    ya funciono el angular pintado de api antes de la tormenta?

--- src/app/components/cards/cards.component.ts
+++ src/app/components/cards/cards.component.ts
@@ -1,15 +1,21 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit  } from '@angular/core';
+import { CharactersService } from 'src/app/services/characters.service';
 
 @Component({
   selector: 'app-cards',
   templateUrl: './cards.component.html',
-  styleUrls: ['./cards.component.css']
+  styleUrls: ['./cards.component.css'],
 })
-export class CardsComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
+export class CardsComponent {
+  characters: any[] = [];
+  constructor(private api: CharactersService) {
+    
   }
-
+  ngOnInit(): void {
+    this.api.getCharacters().subscribe((data: any) => {
+      this.characters = data.results;
+      console.log(this.characters)
+    });
+    console.log(this.characters)
+  }
 }
---
+++ src/app/services/characters.service.ts
@@ -0,0 +1,17 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class CharactersService {
+  constructor(private http: HttpClient) {
+
+  }
+  getCharacters(){
+    return this.http.get("https://rickandmortyapi.com/api/character");
+    
+
+  }
+
+}
